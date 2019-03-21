afa3ef8ae2556eceaab3deb71e4a34a88a5d1828
Byron Perpetua <byronperpetua@gmail.com>
2019-03-21 16:37:03 -0400

    game working

--- hearts.py
+++ hearts.py
@@ -31,6 +31,14 @@ class Card:
     def __repr__(self):
         return self.rank + self.suit
 
+    def points(self):
+        if self.suit == 's' and self.rank == 'q':
+            return 13
+        elif self.suit == 'h':
+            return 1
+        else:
+            return 0 
+
 
 class Hand:
     def __init__(self, cards='', full_deck=False):
@@ -47,6 +55,9 @@ class Hand:
             s1 += suit + ' ' * max((len(in_suit)), 1)
             s2 += ''.join(in_suit) + ' ' * max(2 - len(in_suit), 1)
         return s1 + s2
+
+    def append(self, card):
+        self.cards.append(card)
     
     def contains(self, card):
         return card in self.cards
@@ -62,12 +73,24 @@ class Hand:
     def extend(self, other):
         for c in other.cards:
             self.cards.append(c)
+    
+    def has_only_point_cards(self):
+        return all([c.points() > 0 for c in self.cards])
 
-    def get_cards(self):
-        return self.cards
+    def has_only_suit(self, suit):
+        return all([c.suit == suit for c in self.cards])
+
+    def is_void(self, suit):
+        return all([c.suit != suit for c in self.cards])
 
     def length(self):
         return len(self.cards)
+
+    def points(self):
+        return sum([c.points() for c in self.cards])
+
+    def remove(self, card):
+        self.cards.remove(card)
 
     def remove_hand(self, other):
         for c in other.cards:
@@ -87,12 +110,14 @@ class Hand:
             other_copy.remove(c)
         return True
 
+
 class Round:
     def __init__(self, server, pass_dir, num_tricks=13):
         self.num_players = len(server.players)
         self.num_tricks = num_tricks
         self.scores = [0] * self.num_players
         self.hands = []
+        self.hearts_broken = False
         self.passes = []
         self.server = server
         self.pass_dir = pass_dir
@@ -105,52 +130,112 @@ class Round:
     # TODO: threads
     def get_pass(self):
         msg = 'Choose 3 cards to pass ' + self.pass_dir + ': '
-        print(self.hands[0])
-        while True:
-            h = Hand(input(msg))
-            if h.length() == 3 and h.subset_of(self.hands[0]):
-                self.passes.append(h)
-                break
-        for i in range(1, self.num_players):
-            self.server.send(self.server.players[i], str(self.hands[i]))
+        for i in range(self.num_players):
+            self.server.send(i, str(self.hands[i]))
             while True:
-                h = Hand(self.server.request(self.server.players[i], msg))
+                try:
+                    h = Hand(self.server.request(i, msg))
+                except (TypeError, IndexError):
+                    continue
                 if h.length() == 3 and h.subset_of(self.hands[i]):
                     self.passes.append(h)
+                    self.hands[i].remove_hand(self.passes[i])
                     break
-        for i in range(self.num_players):
-            self.hands[i].remove_hand(self.passes[i])
 
     def distribute_pass(self):
-        if self.pass_dir != 'hold':
-            for i in range(self.num_players):
-                if self.pass_dir == 'left':
-                    received = self.passes[(i+1) % self.num_players]
-                elif self.pass_dir == 'right':
-                    received = self.passes[(i-1) % self.num_players]
-                elif self.pass_dir == 'across':
-                    received = self.passes[(i+2) % self.num_players]
-                self.hands[i].extend(received)
-                if i == 0:
-                    print('Received: ' + received.short_str())
-                    print(self.hands[0])
+        for i in range(self.num_players):
+            if self.pass_dir == 'left':
+                received = self.passes[(i+1) % self.num_players]
+            elif self.pass_dir == 'right':
+                received = self.passes[(i-1) % self.num_players]
+            elif self.pass_dir == 'across':
+                received = self.passes[(i+2) % self.num_players]
+            self.hands[i].extend(received)
+            self.server.send(i, 'Received: ' + received.short_str())
+    
+    def validate_play(self, card, hand, suit_led):
+        if hand.contains(card):
+            if suit_led is None:
+                if self.trick_num == 1:
+                    return card == Card('2c')
                 else:
-                    self.server.send(self.server.players[i],
-                                    'Received: ' + received.short_str())
-                    self.server.send(self.server.players[i], str(self.hands[i]))
+                    if card.suit == 'h':
+                        return self.hearts_broken or hand.has_only_suit('h')
+                    else:
+                        return True
+            else:
+                if card.suit == suit_led:
+                    return True
+                else:
+                    if hand.is_void(suit_led):
+                        if self.trick_num == 1:
+                            return (card.points() == 0
+                                    or hand.has_only_point_cards())
+                        else:
+                            return True
+                    else:
+                        return False
+        else:
+            return False
 
     def play_trick(self):
-        pass
+        trick = Hand()
+        suit_led = None
+        for i in range(self.num_players):
+            self.server.send(i, str(self.hands[i]))
+        for k in range(self.num_players):
+            i = (self.leader + k) % self.num_players
+            while True:
+                try:
+                    msg = 'Play a card: '
+                    c = Card(self.server.request(i, msg))
+                except (TypeError, IndexError):
+                    continue
+                if self.validate_play(c, self.hands[i], suit_led):
+                    self.hands[i].remove(c)
+                    trick.append(c)
+                    msg = self.server.players[i].username + ': ' + str(c)
+                    self.server.broadcast(msg, exclude=i)
+                    break
+            if suit_led is None:
+                suit_led = c.suit
+                winner = i
+                winning_card = c
+            if c.suit == suit_led and c > winning_card:
+                winner = i
+                winning_card = c
+            if c.suit == 'h' and not self.hearts_broken:
+                self.hearts_broken = True
+        msg = self.server.players[winner].username + ' takes the trick.'
+        self.server.broadcast(msg)
+        self.scores[winner] += trick.points()
+        self.leader = winner
 
     def play(self):
         self.deal()
-        self.get_pass()
-        self.distribute_pass()
+        if self.pass_dir != 'hold':
+            self.get_pass()
+            self.distribute_pass()
         self.leader = [h.contains(Card('2c')) for h in self.hands].index(True)
-        for i in range(self.num_tricks):
+        self.trick_num = 1
+        while self.trick_num <= 13:
             self.play_trick()
-        # TODO: fix
-        self.scores = [0, 26, 26, 26]
+            self.trick_num += 1
+        if max(self.scores) == 26:
+            shooter = self.scores.index(26)
+            msg = 'Enter "a" to add or "s" to subtract: '
+            while True:
+                response = self.server.request(shooter, msg)
+                if response == 'a':
+                    for i in range(self.num_players):
+                        if i == shooter:
+                            self.scores[i] = 0
+                        else:
+                            self.scores[i] = 26
+                    break
+                elif response == 's':
+                    self.scores[shooter] = -26
+                    break
 
 
 class Game:
@@ -164,22 +249,24 @@ class Game:
         self.pass_dirs = pass_dirs
         self.pass_dir_num = 0
 
-    def get_scores_str(self):
+    def get_scores_str(self, scores):
         s = ''
         for i in range(self.num_players):
-            s += (self.server.players[i].username + ': ' + str(self.scores[i])
+            s += (self.server.players[i].username + ' ' + str(scores[i])
                   + '    ')
         return s
 
     def play(self):
         while max(self.scores) < self.max_score:
-            self.server.broadcast('Starting round ' + str(self.round_num),
-                                  print_=True)
+            self.server.broadcast('Starting round ' + str(self.round_num) + '.')
             round = Round(self.server, self.pass_dirs[self.pass_dir_num])
             round.play()
+            self.server.broadcast('Round scores:    '
+                                  + self.get_scores_str(round.scores))
             for i in range(self.num_players):
                 self.scores[i] += round.scores[i]
-            self.server.broadcast(self.get_scores_str(), print_=True)
+            self.server.broadcast('Game scores:    '
+                                  + self.get_scores_str(self.scores))
             self.round_num += 1
             self.pass_dir_num = (self.pass_dir_num + 1) % len(self.pass_dirs)
                 
@@ -193,23 +280,28 @@ class Server:
         self.sock.bind(('', self.port))
         self.sock.listen()
 
-    def send(self, player, msg):
-        player.conn.sendall(msg.encode('utf-8'))
+    def send(self, player_num, msg):
+        if player_num == 0:
+            print(msg)
+        else:
+            self.players[player_num].conn.sendall(msg.encode('utf-8'))
         sleep(0.1)
 
-    def broadcast(self, msg, exclude=(), print_=False):
-        for i in range(1, len(self.players)):
-            if i not in exclude:
-                self.send(self.players[i], msg)
-        if print_:
-            print(msg)
+    def broadcast(self, msg, exclude=None):
+        for i in range(len(self.players)):
+            if i != exclude:
+                self.send(i, msg)
 
     def receive(self, player):
         return player.conn.recv(self.bufsize).decode('utf-8')
 
-    def request(self, player, msg):
-        self.send(player, msg + '\x05')
-        return self.receive(player)
+    def request(self, player_num, msg):
+        if player_num == 0:
+            return input(msg)
+        else:
+            self.send(player_num, msg + '\x05')
+            player = self.players[player_num]
+            return player.conn.recv(self.bufsize).decode('utf-8')
 
     def start_game(self):
         self.players[0].username = input('Enter username: ')
@@ -218,7 +310,7 @@ class Server:
             new_player = Player(self.sock.accept()[0])
             self.players.append(new_player)
             new_player.username = self.receive(new_player)
-            self.broadcast(new_player.username + ' has joined.', print_=True)
+            self.broadcast(new_player.username + ' has joined.')
         game = Game(self)
         game.play()
 
@@ -249,7 +341,11 @@ class Client:
                 print('Disconnected.')
                 break
             elif msg.endswith('\x05'):
-                self.send(input(msg))
+                while True:
+                    response = input(msg)
+                    if response:
+                        self.send(response)
+                        break
             else:
                 print(msg)
 
